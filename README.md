5e3536bfdfbf6db2a64341e791a4b48a550e98e0
Martin Edlman <ac@an.y-co.de>
2019-06-28 10:40:00 +0200

    Platformio (Arduino) app to test MPU6050 accelerometer with ESP32
    Signed-off-by: Martin Edlman <ac@an.y-co.de>

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # platformio-mpu6050-test
 Test of MPU6050 accelerometer/gyroscope
+
+## IDE, Libraries
+- Platformio (Atom IDE)
+- I2Cdevlib-Core_ID11
+- I2Cdevlib-MPU6050_ID107
+
+## Hardware
+- ESP32 chip
+- MPU6050 accelerometer
+
+## Setup
+I have MPU6050 interrupt pin connected to ESP32 (Wemos D1 mini) GPIO34 pin to
+detect motion and zero motion interrupts. I don't need (and I don't want) to
+read accelerometer data in each loop() call.
+
+I observe some strange behaviour.
+1. no matter what threshold I set, it's still too sensitive and gives interrupt
+   on little movement (even max value 255).
+2. when the duration is 9 (or less) the interrupt is fired even on little
+   movement. When it's 10 (or more) the interrupt is not fired no matter how
+   hard I shake the device.
+3. same applies to wakeup. I set the ESP32 to wake up on interrupt on PIN 34
+   and put it to sleep. It wakes up on little movement. I tried various
+   thresholds and durations.
+
+```
+    accelerometer.setIntMotionEnabled(true);
+    accelerometer.setMotionDetectionThreshold(255);
+    accelerometer.setMotionDetectionDuration(9);
+```
---
+++ src/app.cpp
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+
+#include <I2Cdev.h>
+#include <MPU6050.h>
+
+#define ACCEL_INT_GPIO_SEL GPIO_SEL_34
+#define ACCEL_INT_GPIO_NUM GPIO_NUM_34
+
+MPU6050 accelerometer;
+
+uint8_t range = MPU6050_ACCEL_FS_16; // scale 16g
+float scale = 32768.0 / 16.0; // scale of 1g, 16.0 matches range (FS_16)
+//uint8_t range = MPU6050_ACCEL_FS_8; // scale 8g
+//float scale = 32768.0 / 8.0; // scale of 1g, 8 matches range (FS_8)
+//uint8_t range = MPU6050_ACCEL_FS_4; // scale 4g
+//float scale = 32768.0 / 4.0; // scale of 1g, 4.0 matches range (FS_4)
+//uint8_t range = MPU6050_ACCEL_FS_2; // scale 2g
+//float scale = 32768.0 / 2.0; // scale of 1g, 2 matches range (FS_2)
+int count = 3;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(ACCEL_INT_GPIO_NUM, INPUT);
+
+  esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+  uint64_t mask = cause == ESP_SLEEP_WAKEUP_EXT1 ? esp_sleep_get_ext1_wakeup_status() : 0;
+  bool wake_up_accel = (mask & ACCEL_INT_GPIO_SEL) == ACCEL_INT_GPIO_SEL;
+  Serial.printf("wakeup cause %i, mask %llu, wu accel %i\n", cause, mask, wake_up_accel);
+
+  Serial.println("Initializing I2C devices...");
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+      Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+      Fastwire::setup(400, true);
+  #endif
+  accelerometer.initialize();
+  delay(500);
+
+  bool motionDetected = accelerometer.getIntMotionStatus() == 1;
+  bool zeroMotionDetected = accelerometer.getIntZeroMotionStatus() == 1;
+  Serial.printf("Accelerometer::getWakeUpReason motionDetected %d, zeroMotionDetected %d\n", motionDetected, zeroMotionDetected);
+
+  // verify connection
+  Serial.println("Testing device connections...");
+  if(accelerometer.testConnection())
+    Serial.println("MPU6050 connection successful");
+  else
+    Serial.println("MPU6050 connection failed");
+
+  accelerometer.setDHPFMode(MPU6050_DHPF_RESET); // reset
+  delay(100);
+  accelerometer.setFullScaleAccelRange(range);
+
+  accelerometer.setIntZeroMotionEnabled(true);
+  accelerometer.setZeroMotionDetectionThreshold(100);
+  accelerometer.setZeroMotionDetectionDuration(3);
+  accelerometer.setIntMotionEnabled(true);
+  accelerometer.setMotionDetectionThreshold(255);
+  accelerometer.setMotionDetectionDuration(8);
+
+  Serial.printf("Range %d, scale %f\n", range, scale);
+}
+
+void loop() {
+  int16_t ax, ay, az;
+  //accelerometer.getAcceleration(&ax, &ay, &az);
+  //Serial.printf("motion ax %fg, ay %fg, az %fg\n", ax / scale, ay / scale, az / scale);
+  //delay(300);
+  int mpuIntStatus = accelerometer.getIntStatus();
+  bool motionDetected = mpuIntStatus & _BV(MPU6050_INTERRUPT_MOT_BIT);
+  bool zeroMotionDetected = mpuIntStatus & _BV(MPU6050_INTERRUPT_ZMOT_BIT);
+  //bool motionDetected = accelerometer.getIntMotionStatus() == 1;
+  //bool zeroMotionDetected = accelerometer.getIntZeroMotionStatus() == 1;
+  if(motionDetected || zeroMotionDetected)
+    Serial.printf("motion detected %d, zero motion detected %d\n", motionDetected, zeroMotionDetected);
+  if(motionDetected) {
+    accelerometer.getAcceleration(&ax, &ay, &az);
+    Serial.printf("#%i motion detected ax %fg (%d), ay %fg (%d), az %fg (%d)\n", count, ax / scale, ax, ay / scale, ay, az / scale, az);
+    count--;
+  }
+
+  if(count < 0) {
+    // sleep
+    Serial.println("sleep");
+    delay(500);
+    accelerometer.setMotionDetectionThreshold(15);
+    accelerometer.setMotionDetectionDuration(7);
+    accelerometer.setIntMotionEnabled(true);
+    accelerometer.setZeroMotionDetectionThreshold(0);
+    accelerometer.setZeroMotionDetectionDuration(0);
+    accelerometer.setIntZeroMotionEnabled(false);
+    delay(100);
+    esp_sleep_enable_ext1_wakeup(ACCEL_INT_GPIO_SEL, ESP_EXT1_WAKEUP_ANY_HIGH);
+    delay(100);
+
+    esp_deep_sleep_start();
+  }
+
+}
